4862bcf0b63227dd7f479119e6b73ecf8715a3dd
ArmenMesropyan <mesropyan200315@gmail.com>
2020-05-21 02:48:36 +0400

    Datepickers added

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -1,4 +1,6 @@
 import 'materialize-css/dist/js/materialize.min';
+import '../../css/materialize.css';
+
 // init Autocomplete
 
 const autocomplete = document.querySelectorAll('.autocomplete');
@@ -10,7 +12,10 @@ window.M.Autocomplete.init(autocomplete, {
 // init DatePickers
 
 const datepickers = document.querySelectorAll('.datepicker');
-window.M.Datepicker.init(datepickers, {});
+window.M.Datepicker.init(datepickers, {
+    yearRange: 2,
+    showClearBtn: true,
+});
 
 export default function getAutocompleteInstance(elem) {
     const instance = window.M.Autocomplete.getInstance(elem);
